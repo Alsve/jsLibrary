30a427c1ab8cfae1183230b03e3ba73eecc0fa85
Alsve <snowshoewolf@gmail.com>
2018-01-07 20:37:46 +0700

    Update : +asynchronous function returning promise

--- terbilang.js
+++ terbilang.js
@@ -3,182 +3,213 @@
 // License       : BSD License v3
 // Repositories  : https://github.com/Alsve/jsLibrary/
 
-   // Membuat kelas
-    var _Terbilang = {}
+var Terbilang = function(input) {
+    this.in = isNaN(input) ? 0 : input;
+    this.out = "";
+}
+
+Terbilang.async = function(input, callback) {
+    return Promise.resolve(new Terbilang(input))
+                  .then(x => {x.what(); return x;})
+                  .then(x => {callback(x); return x;});
+}
+
+// CONSTANT DECLARATION
+// Decimal precision and formatting
+Terbilang.PRECISION = 2;
+
+// Zero dan comma locale
+Terbilang.null = "nol"
+Terbilang.comma = "koma"
+
+// Delimiter
+Terbilang.delim = '.'
+// Decimal Delimiter
+Terbilang.decDelim = ','
+
+// Units locale for value less than thousand
+Terbilang.unitsThousand = ["ratus", "puluh", ""];
+
+// Units locale for every thousand
+Terbilang.units = [ ""          ,"ribu"      ,"juta" 
+                   ,"milyar"    ,"triliun"   ,"kuadriliun"
+                   ,"kuantiliun","sekstiliun","septiliun"
+                   ,"oktiliun"  ,"noniliun"  ,"desiliun"   ];
+  
+// Number locale
+Terbilang.numberWords = [ ""       ,"satu"    ,"dua" ,"tiga" 
+                         ,"empat"  ,"lima"    ,"enam","tujuh"
+                         ,"delapan","sembilan"                ];
     
-    var Terbilang = _Terbilang;
-    
-    // Deklarasi variable kelas
-    _Terbilang.comma = 0.0;
-    _Terbilang.arr = [];
-    
-    // DEKLARASI KONSTANTA
-    // Pemisah perseribu
-    _Terbilang.delimiter = '.'
-    // Pemisah desimal
-    _Terbilang.decimalDelimiter = ','
-    // Format angka belakang koma
-    _Terbilang.PRESISI_DESIMAL = 2;
-    
-    // Mengembalikan unit satuan khusus methode this.seribu(input)
-    _Terbilang.unitSeribu = ["", "puluh", "ratus"];
-    // Unit satuan dalam Bahasa Indonesia
-    _Terbilang.units = [ ""          ,"ribu"      ,"juta" 
-                       ,"milyar"    ,"triliun"   ,"kuadriliun"
-                       ,"kuantiliun","sekstiliun","septiliun"
-                       ,"oktiliun"  ,"noniliun"  ,"desiliun"   ];
-    
-    // Mengembalikan angka dalam Bahasa Indonesia
-    _Terbilang.angka = [ ""       ,"satu"    ,"dua" ,"tiga" 
-                        ,"empat"  ,"lima"    ,"enam","tujuh"
-                        ,"delapan","sembilan"                ];
-    
-    // Khusus untuk methode this.seribu(input)
-    _Terbilang.replaceWith = {"satu ratus"         : "seratus",
-                              "satu puluh satu"    : "sebelas",
-                              "satu puluh dua"     : "dua belas",
-                              "satu puluh tiga"    : "tiga belas",
-                              "satu puluh empat"   : "empat belas",
-                              "satu puluh lima"    : "lima belas",
-                              "satu puluh enam"    : "enam belas",
-                              "satu puluh tujuh"   : "tujuh belas",
-                              "satu puluh delapan" : "delapan belas",
-                              "satu puluh sembilan": "sembilan belas",
-                              "satu puluh"         : "sepuluh"};
-    
-    // khusus untuk methode this.apa(input)
-    // Dibedakan supaya tidak berat perulangannya
-    _Terbilang._replaceWith = {"satu ribu"         : "seribu"};
-    
-    // Mengembalikan string seribu dalam B. Indonesia
-    _Terbilang.seribu = function(input) {
-      if (input > 999 || isNaN(input)) return "";
-      
-      input = parseInt(input);
-      
-      var len = (input + "").length, i;
-      var digArr = [], ret = [], strret = "";
-      
-      // memecah integer menjadi array digit
-      for (i = 0; i < len; i++) {
-        digArr.push(input % 10);
-        input = parseInt(input / 10);
-      }
-      
-      // membuat string seribuan
-      for (i = len; i > 0;) {
-        if (digArr[i] !== 0 && (i + 1) <= len)
-          ret.push(this.unitSeribu[i]);
-        ret.push(this.angka[digArr[--i]]);
-      }
-      
-      strret = ret.join(" ");
-      
-      // Membetulkan string
-      for (var key in this.replaceWith) {
-        if (this.replaceWith.hasOwnProperty(key))
-          strret = strret.replace(key, this.replaceWith[key]);
-      }
-      
-      return strret;
-    }
-    
-    // Mengembalikan string koma dalam Bahasa Indonesia
-    _Terbilang.koma = function(input) {
-      if (isNaN(input)) return "";
-      input = input - Math.floor(input);
-      input = input.toPrecision(this.PRESISI_DESIMAL);
-      input = input.split(".").slice(1)[0];
-      
-      // Deklarasi variable
-      var strret = "koma ", i;
-      
-      if (input.length === 0) return "";
-      
-      // Membuat string koma
-      for (i = 0; i < input.split('').length; i++) {
-        var temp = parseInt(input.charAt(i));
-        if (temp !== 0) strret += " " + this.angka[temp];
-        else strret += " nol";
-      }
-           
-      return strret;
-    }
-    
-    // Mengembalikan string dari angka input dalam B.Indonesia
-    _Terbilang.apa = function(inp) {
-      // Deklarasi variable
-      var strret = "", i, seribuArr = [], ret = [], input;
-      
-      // Memecah integer menjadi array perseribu terbalik
-      // input => 123000 => [0, 123]
-      // input => 123321 => [321, 123]
-      inp = inp.replace(',', '.')
-               .split('.');
-      
-      input = inp[0];
-      this.comma = parseFloat("0." + inp[1])
-                     .toPrecision(this.PRESISI_DESIMAL);
-      
-      
-      for (i = input.length - 1; i >= 0; i -= 3)
-        seribuArr.push(input.charAt(i - 2) 
-                     + input.charAt(i - 1)
-                     + input.charAt(i));
-      
-      
-      // Membuat string dari seribuArr
-      for (i = seribuArr.length; i > 0;) {
-        if (seribuArr[i] !== 0 && (i + 1) <= seribuArr.length)
-          ret.push(this.units[i]);
-        ret.push(this.seribu(seribuArr[--i]));
-      }
-      
-      strret = ret.join(' ');
-      // Tambahkan koma
-      strret += " " + this.koma(this.comma);
-      
-      // Membetulkan string seribu
-      for (var key in this._replaceWith) {
-        if (this._replaceWith.hasOwnProperty(key))
-          strret = strret.replace(key, this._replaceWith[key]);
-      }
-      
-      return strret;
-    }
-    
-    _Terbilang.format = function(inp) {
-      inp = inp.replace(',', '.');
-      if (isNaN(inp)) return "";
-      
-      // Deklarasi variable
-      var strret = "", i, seribuArr = [], ret = [], input;
-      
-      // Memecah integer menjadi array perseribu terbalik
-      // input => 123000 => [0, 123]
-      // input => 123321 => [321, 123]
-      inp = inp.split('.');
-      
-      input = inp[0];
-      this.comma = parseFloat("0." + inp[1])
-                     .toPrecision(this.PRESISI_DESIMAL);
-      
-      
-      for (i = input.length - 1; i >= 0; i -= 3)
-        seribuArr.push(input.charAt(i - 2) 
-                     + input.charAt(i - 1)
-                     + input.charAt(i));
-      
-      seribuArr.reverse();
-      ret.push(seribuArr[0]);
-      
-      for (i = 1; i < seribuArr.length; i++)
-        ret.push(this.delimiter + seribuArr[i]);
-      
-      ret.push(this.decimalDelimiter 
-            + (inp[1] === undefined ? '0' : inp[1]));
-      
-      strret = ret.join('');
-      
-      return strret;
-    }
+// Replacement string
+Terbilang.replaceWith = {"satu ratus"         : "seratus",
+                         "satu puluh satu"    : "sebelas",
+                         "satu puluh dua"     : "dua belas",
+                         "satu puluh tiga"    : "tiga belas",
+                         "satu puluh empat"   : "empat belas",
+                         "satu puluh lima"    : "lima belas",
+                         "satu puluh enam"    : "enam belas",
+                         "satu puluh tujuh"   : "tujuh belas",
+                         "satu puluh delapan" : "delapan belas",
+                         "satu puluh sembilan": "sembilan belas",
+                         "satu puluh"         : "sepuluh",
+                         "satu ribu"          : "seribu"};
+
+/*
+ * Sync version of Terbilang.prototype.thousand
+ */
+Terbilang.thousand = function(input) {
+    return Terbilang.prototype.thousand.apply(null, [input]);
+}
+
+/*
+ * Sync version of Terbilang.prototype.decimal
+ */
+Terbilang.decimal = function(input) {
+    return Terbilang.prototype.decimal.apply(null, [input]);
+}
+
+/*
+ * Sync version of Terbilang.prototype.rectify
+ */
+Terbilang.rectify = function(input) {
+    return Terbilang.prototype.rectify.apply(null, [input]);
+}
+
+/*
+ * Sync version of Terbilang.prototype.what
+ */
+Terbilang.what = function(input) {
+    input = Terbilang.prototype
+              .core.thousandSplit.apply(null, [input.split(".")[0]])
+              .map(x => Terbilang.thousand(x))
+              .map((x, i) => x === "" ? "" : x + " " + Terbilang.units[i])
+              .reverse().join(" ");
+    input += " " + Terbilang.decimal(input.split(".")[1]);
+  
+    return (input = Terbilang.rectify(
+                        input.replace(/\s+/g, " ")
+                             .replace(/\s$/, ""))
+           );
+}
+
+/*
+ * Sync version of Terbilang.prototype.format
+ */
+Terbilang.format = function(input) {
+    var inp = input.split(".");
+        inp[0] = Terbilang.prototype.core
+                          .thousandSplit.apply(null, [inp[0]]);
+    return inp[0].reverse().join(Terbilang.delim) +
+            (inp[1] ? (Terbilang.decDelim 
+                      + inp[1] + "0".repeat(Terbilang.PRECISION))
+            .slice(0, Terbilang.PRECISION + 1) : 
+            Terbilang.PRECISION === 0 ? "" : 
+            Terbilang.decDelim + "0".repeat(Terbilang.PRECISION));
+}
+
+/*
+ * Core functor that transform and return raw and unrectified words
+ * @args : <Integer>
+ * @return : <String> raw and unrectified words
+ */
+Terbilang.prototype.core = function(input) {
+    return this.core.thousandSplit(input)
+      .map(x => Terbilang.thousand(x))
+      .map((x, i) => x === "" ? "" : x + " " + Terbilang.units[i])
+      .reverse().join(" ");
+}
+
+Terbilang.prototype.core.thousandSplit = function(input) {
+  var temp = "";
+  return (input + "").split("").reverse()
+      .reduce((p, c, i, a) => {
+		if (a.length == (i + 1))
+			return p.concat(temp + c)
+            
+		if ((i + 1) % 3 === 0) {
+			let pp = p.concat(temp + c);
+			temp = "";
+			return pp;
+        }
+		temp += c;
+		return p;
+    }, []).map(x => x.split("").reverse().join(""));
+}
+
+/*
+ * Thousand functor transform 
+ * @args : <Integer>
+ * @return : <String>
+ */
+Terbilang.prototype.thousand = function(input) {
+    if (input > 999) return "";
+    if (input == 0) return "";
+  
+    return ("0".repeat(3) + input).slice(-3).split("").map((x, i) => {
+            if (x === "0") return "";
+
+            return Terbilang.numberWords[parseInt(x)] +
+                   " " +
+                   Terbilang.unitsThousand[i];
+            }).filter(x => x !== "").join(" ");
+}
+
+/*
+ * Decimal functor transform decimal number into words
+ * @args : <Integer>
+ * @return : <String> Word of number
+ */
+Terbilang.prototype.decimal = function(input) {
+    if (Terbilang.PRECISION === 0) return "";
+    return Terbilang.comma + " "  + 
+           ((input ? input : "") + "0".repeat(Terbilang.PRECISION))
+               .slice(0, Terbilang.PRECISION).split("")
+               .map(x => {
+                   if (x === "0") return Terbilang.null;
+                   return Terbilang.numberWords[parseInt(x)];
+               }).join(" ");
+}
+
+/*
+ * Rectify functor replace input string that include key word 
+ *     to value word in Terbilang.replaceWith.
+ * @args : <String> 
+ * @return : <String> The rectified string object.
+ */
+Terbilang.prototype.rectify = function(input) {
+    Object.keys(Terbilang.replaceWith)
+      .forEach(x => input = input.replace(new RegExp(x, 'g'), 
+                                          Terbilang.replaceWith[x]));
+  
+    return input;
+}
+
+/*
+ * Format functor return string number with thousand delimitor
+ * @return : <String>
+ */
+Terbilang.prototype.format = function() {
+    var inp = this.in.split(".");
+        inp[0] = this.core.thousandSplit(inp[0]);
+    return inp[0].reverse().join(Terbilang.delim) +
+            (inp[1] ? (Terbilang.decDelim 
+                      + inp[1] + "0".repeat(Terbilang.PRECISION))
+            .slice(0, Terbilang.PRECISION + 1) : 
+            Terbilang.PRECISION === 0 ? "" : 
+            Terbilang.decDelim + "0".repeat(Terbilang.PRECISION));
+}
+
+/*
+ * Rewrite this.out as a result,
+ * @return : <String> Word of number from this.in
+ */
+Terbilang.prototype.what = function() {
+    this.out = this.core(this.in.split(".")[0]);
+    this.out += " " + this.decimal(this.in.split(".")[1]);
+  
+    return (this.out = this.rectify(
+                        this.out.replace(/\s+/g, " ")
+                                .replace(/\s$/, ""))
+           );
+}
